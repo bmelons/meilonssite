4a8a67d5884111392e03ddb1b4a8e67673822255
bmelons <brizfauxfox@gmail.com>
2025-11-29 20:18:49 -0600

    for

--- frontend/src/Header.vue
+++ frontend/src/Header.vue
@@ -1,13 +1,19 @@
 <template>
   <div class="box content header">
-    <h1 class="site-name">gaykitty.online</h1>
-    <div class="hlist">
+    <h1 class="site-name">gaykitty.<span style="color:plum">online</span></h1>
+    <h2 class="subtitle">home of the homo</h2>
+    <div id="buttonbox" class="hlist">
       <button v-for="txt in buttons" v-bind:id="txt" @click="clickHandle">{{ txt }}</button>
     </div>
   </div>
 </template>
 
 <script lang="ts">
+import { computed } from "vue";
+const isMobile = computed(() => {
+  return screen.width <= 760;
+});
+
 export default {
   data() {
     return {
